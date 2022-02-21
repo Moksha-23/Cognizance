7b4815a552bb2f0812f8a8970dc68794eb0a64f6
Moksha-23 <99631409+Moksha-23@users.noreply.github.com>
2022-02-21 21:49:45 +0530

    Delete Task-4 directory

--- Task-4/Question-1/Q-1(addition on two user input numbers).py
+++
@@ -1,5 +0,0 @@
-# addition on two user input numbers
-a = int(input('First number : '))
-b = int(input('Second number : '))
-sum = a + b
-print('sum = ',sum)
--- Task-4/Question-2/Q-2(displaying even index chatacters).py
+++
@@ -1,3 +0,0 @@
-#displaying characters that are present at an even index number.
-string = input('Enter the string :')
-print('Even characters:',string[0::2])
